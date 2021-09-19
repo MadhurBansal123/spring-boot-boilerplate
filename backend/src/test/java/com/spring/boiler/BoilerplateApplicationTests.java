d5e60d2ea0a3311ecea54616d49a3aeaeba0c280
MadhurBansal123 <madhur.bansal0016@gmail.com>
2021-09-19 18:18:01 +0530

    unit test case added

--- backend/src/test/java/com/spring/boiler/BoilerplateApplicationTests.java
+++ backend/src/test/java/com/spring/boiler/BoilerplateApplicationTests.java
@@ -1,13 +1,25 @@
 package com.spring.boiler;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class BoilerplateApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mvc;
 
+	@Test
+	public void checkHealth() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/healthCheck").accept(MediaType.APPLICATION_JSON))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.content().string("I am running Healthy!! What about you??"));
+	}
 }
